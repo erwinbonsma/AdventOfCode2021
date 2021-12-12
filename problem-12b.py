122ed0ce14a2a0a5005b4ee375f2ce7ed45aea75
Erwin Bonsma <erwin.bonsma@topic.nl>
2021-12-12 09:51:21 +0100

    Solve Problem 12B

--- problem-12b.py
+++ problem-12b.py
@@ -1,31 +1,34 @@
 paths = {}
 
-with open("test-12.txt") as fp:
+with open("input-12.txt") as fp:
     for line in fp:
         line = line.strip()
         a, b = line.split('-')
         paths.setdefault(a, []).append(b)
         paths.setdefault(b, []).append(a)
 
-def find_paths(node = 'start', visit_counts = {}, total_paths = 0, path = []):
+def find_paths(node = 'start', visited = set(), total_paths = 0, repeat = None, path = []):
+    #print(path, node, visited, repeat)
     is_small = node.islower()
     if is_small:
         if node == 'end':
-            print("==> ", path)
             return total_paths + 1
-        max_count = 1 if node == 'start' else 2
-        if visit_counts.setdefault(node, 0) == max_count:
-            return total_paths
-        visit_counts[node] += 1
+        if node in visited:
+            if repeat is None and node != 'start':
+                repeat = node
+            else:
+                return total_paths
+        else:
+            visited.add(node)
     path.append(node)
 
     for next_node in paths[node]:
-        total_paths = find_paths(next_node, visit_counts, total_paths, path)
-    
-    if is_small:
-        visit_counts[node] -= 1
-    path.pop()
+        total_paths = find_paths(next_node, visited, total_paths, repeat, path)
 
+    path.pop()    
+    if is_small:
+        if repeat != node:
+            visited.remove(node)
     return total_paths
 
 print(find_paths())
