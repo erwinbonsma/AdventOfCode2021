65e72f948020e012452dc7d9b837236c83bcbb50
Erwin Bonsma <erwin.bonsma@topic.nl>
2021-12-21 19:16:41 +0100

    Improve solution for Problem 21B

--- problem-21b.py
+++ problem-21b.py
@@ -9,19 +9,26 @@ outcomes = collections.Counter(
     3 + a + b + c for a, b, c in itertools.product(range(3), range(3), range(3))
 )
 
-def update_score(pos_a, pos_b, score_a, score_b, count):
+def sum_tuples(tuple1, tuple2):
+    return tuple1[0] + tuple2[0], tuple1[1] + tuple2[1]
+
+def multiply(num, tuple):
+    return tuple[0] * num, tuple[1] * num
+
+@functools.cache
+def update_score(pos_a, pos_b, score_a, score_b):
     score_a += pos_a + 1
     if score_a >= target_score:
-        return count, 0
-    wins_b, wins_a = play(pos_b, pos_a, score_b, score_a, count)
+        return 1, 0
+    wins_b, wins_a = play(pos_b, pos_a, score_b, score_a)
     return wins_a, wins_b
 
-def play(pos_a, pos_b, score_a, score_b, count = 1):
+def play(pos_a, pos_b, score_a, score_b):
     return functools.reduce(
-        lambda x, y: (x[0] + y[0], x[1] + y[1]), (
-            update_score(
-                (pos_a + die_sum) % num_pos, pos_b, score_a, score_b, count * occurences
-            ) for die_sum, occurences in outcomes.items()
+        sum_tuples, (
+            multiply(occurences, update_score(
+                (pos_a + die_sum) % num_pos, pos_b, score_a, score_b
+            )) for die_sum, occurences in outcomes.items()
         )
     )
 
